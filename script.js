282eab539265ae8699371f3b0b04feae97f300e0
Turayo2006 <120662177+Turayo2006@users.noreply.github.com>
2023-10-10 09:12:35 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,51 @@
-$(document).ready(function(){
+const bar = document.getElementById('bar')
+const close = document.getElementById('close')
+const nav = document.getElementById('nav-list')
 
-    $nav = $('.nav');
+if (bar) {
+  bar.addEventListener('click', () => {
+    nav.classList.add('active')
+  });
+ 
+ if (close) {
+   close.addEventListener('click', () => {
+     nav.classList.remove('active')
+   })
+ }
+  
+  
+}
+
+
+
     
-    $toggleCollapse = $('.toggle-collapse');
-    
-    /* click event on toggle menu */
-    $toggleCollapse.click(function(){
-    $nav.toggleClass('collapse');
-    });
+// if (search) {
+//   search.addEventListener('click', () => {
+//     nav.classList.add('active')
+//   });
+
+
+
+
+/*--------------------Carousel------------*/
+  
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:true,
+    dots:true,
+  autoplay:true,
+  autoplayTimeout: 1800,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
 });
+  
